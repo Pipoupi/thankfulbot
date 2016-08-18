097a26c0fe7796e42403169b7e6b519ece013ee7
VILLERMOIS Soline <ext.svillermois@cma-cgm.com>
2016-08-18 10:08:53 +0200

    Add heroku variable

--- bot_read.py
+++ bot_read.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-#with the help of http://pythonforengineers.com/
+# with the help of http://pythonforengineers.com/
 
 import praw
 import re
 from config_bot import *
 
 r = praw.Reddit(user_agent = 'bot 0.1 by /u/poupipoupipoupipou')
-r.login(REDDIT_USERNAME, REDDIT_PASS)
+r.login( os.environ['REDDIT_USERNAME'],  os.environ['REDDIT_PASS'])
 
 
 key_word = 'thankfulbot'
