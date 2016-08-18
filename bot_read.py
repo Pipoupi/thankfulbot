eb0b6bbf28841160978599f46606895df7ff23c6
Bruno <brunocauet+github@gmail.com>
2016-08-18 11:27:07 +0100

    Improve bot_read.py idiomaticity
    - pep8
    - 'true'/'false' → True/False
    - terser message creation

--- bot_read.py
+++ bot_read.py
@@ -6,39 +6,37 @@ import praw
 import re
 from config_bot import *
 
-r = praw.Reddit(user_agent = 'bot 0.1 by /u/poupipoupipoupipou')
-r.login( os.environ['REDDIT_USERNAME'],  os.environ['REDDIT_PASS'])
+r = praw.Reddit(user_agent='bot 0.1 by /u/poupipoupipoupipou')
+r.login(os.environ['REDDIT_USERNAME'],  os.environ['REDDIT_PASS'])
 
 
 key_word = 'thankfulbot'
 s = ''
 authorList = []
-first = 'true'
+first = True
 
 with open("posts_replied_to.txt", "r") as f:
         posts_replied_to = f.read()
         posts_replied_to = posts_replied_to.split("\n")
         posts_replied_to = filter(None, posts_replied_to)
-	
+
 def bot_action(c, verbose=True, respond=True):
 	global first
 	global s
 	global authorList
-	
+
 	submission = r.get_submission(url=c.link_url)
 	if submission.id not in posts_replied_to:
 		authorCom = str(c.author)
 		authorPost = str(c.link_author)
 		flat_comments = praw.helpers.flatten_tree(submission.comments)
-		for comment in flat_comments:	
-			if comment.author.name != authorCom and comment.author.name != authorPost:
-				if comment.author.name not in authorList:
-					if first == 'true':
-						s += '/u/'+str(comment.author.name)
-					else:
-						s += ', /u/' + str(comment.author.name)
-					authorList.append(comment.author.name)
-					first = 'false'
+		for comment in flat_comments:
+			if comment.author.name not in authorList + [authorCom, authorPost]:
+				if not first:
+					s += ', '
+				s += '/u/' + str(comment.author.name)
+				authorList.append(comment.author.name)
+				first = False
 		response = ' I\' m a lazy bot. Thank you ' + s
 		if len(authorList) > 1:
 			response += ' for your kind answers !'
@@ -47,7 +45,7 @@ def bot_action(c, verbose=True, respond=True):
 		print(response)
 		c.reply(response)
 		posts_replied_to.append(submission.id)
-	
+
 		with open("posts_replied_to.txt", "w") as f:
 			for post_id in posts_replied_to:
 				f.write(post_id + "\n")
