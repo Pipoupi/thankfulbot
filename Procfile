9e48b2f27f99c1ff41e968240ea70a1672d4c4f1
VILLERMOIS Soline <ext.svillermois@cma-cgm.com>
2016-08-18 15:57:21 +0200

    test

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python bot_read.py runserver 0.0.0.0:5000
+web: python bot_read.py $PORT
--- bot_read.py
+++ bot_read.py
@@ -7,6 +7,11 @@ import re
 import os
 #from config_bot import * no need for heroku version
 
+from intro_to_flask import app
+
+port = int(os.environ.get("PORT", 5000))
+app.run(debug=True, host='0.0.0.0', port=port)
+
 r = praw.Reddit(user_agent='bot 0.1 by /u/poupipoupipoupipou')
 r.login(os.environ['REDDIT_USERNAME'],  os.environ['REDDIT_PASS'])
 
